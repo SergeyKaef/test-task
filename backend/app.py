7399ec6a38ff5afd9cb8a8656d6249964c02f55d
SergeyKaef <maksimchuk.04@inbox.ru>
2026-03-16 14:47:29 +0500

    Добавлено простенькое web-приложение на python

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(b"Hello from Effective Mobile!")
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+def run(port=8080):
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print(f"Server is starting on port {port}...", file=sys.stderr)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        httpd.server_close()
+
+if __name__ == '__main__':
+    run()
